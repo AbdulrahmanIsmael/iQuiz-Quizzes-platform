b84f30631530062306856b02c65844a61b253776
Abdulrahman Ismael <116180351+AbdulrahmanIsmael@users.noreply.github.com>
2025-06-14 00:01:15 +0300

    Refactored some parts of the code

--- src/features/authentication/firebase.ts
+++ src/features/authentication/firebase.ts
@@ -1,16 +1,6 @@
 import { FirebaseApp, initializeApp } from "firebase/app";
 import { Auth, getAuth } from "firebase/auth";
-
-// Firebase Console Configuration Object
-const firebaseConfig = {
-  apiKey: "AIzaSyCCRWsRu_qi7FMvZwcJGnklCSdapMHS58Q",
-  authDomain: "iquiz-website.firebaseapp.com",
-  projectId: "iquiz-website",
-  storageBucket: "iquiz-website.firebasestorage.app",
-  messagingSenderId: "335011489689",
-  appId: "1:335011489689:web:f075121a1f7a132c74d508",
-  measurementId: "G-YEQ7EEHRVR",
-};
+import { firebaseConfig } from "../../constants/firebase-constants";
 
 // Initialize Firebase App
 const app: FirebaseApp = initializeApp(firebaseConfig);
